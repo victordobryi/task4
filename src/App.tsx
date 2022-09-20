6ed549e54e047314ab04f3a83d940eba25b3c0c2
bazla <dobr1nya@mail.ru>
2022-09-20 08:29:43 +0300

    init: start task4

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/main.scss';
+
+export const App = () => (
+  <>
+    <header className='as'>Header</header>
+    <main>Main</main>
+    <footer>Directed by Viktar Kasilkin</footer>
+  </>
+);
+
+export default App;
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false
+      }
+    },
+    historyApiFallback: true
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Vishwas')
+    })
+  ]
+};
